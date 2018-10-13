a4b52a71abc10b5739631a0c39a5d42936d4138c
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-10-13 10:19:59 +0200

    AnimalQuiz

---
+++ AniamlQuiz.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class AniamlQuiz {
+
+
+        public static void main(String[] args) {
+
+            String[] words = {"małpa", "koń", "ryba", "tygrys", "słoń", "papuga"};
+            Scanner scanner = new Scanner(System.in);
+            Random random = new Random();
+
+
+            int fails = 0;
+            int round = 0;
+            String response;
+            while (fails <= 2) {
+
+                String word = words[random.nextInt(words.length)];
+                String hashWord = word;
+
+                for (int i = 0; i < round; i += 5) {
+                    char randomChar = hashWord.charAt(random.nextInt(hashWord.length()));
+                    if (randomChar == '*') {
+                        i--;
+                        continue;
+                    }
+                    hashWord = hashWord.replaceFirst(String.valueOf(randomChar), "*");
+                }
+                System.out.println("Twoje wylosowane zwierzę: " + hashWord);
+                System.out.print("Podaj zwierzę: ");
+                response = scanner.nextLine();
+
+                if (word.equalsIgnoreCase(response)) {
+                    System.out.println("Gratuluje! Zdobywasz punkt");
+                    round++;
+                } else {
+                    System.out.println("Nie udało się odgadnac");
+                    fails++;
+                }
+
+                // write your code here
+            }
+            System.out.println("Koniec gry!!! Twoje punkty to :" + (round));
+        }
+    }
+
+
